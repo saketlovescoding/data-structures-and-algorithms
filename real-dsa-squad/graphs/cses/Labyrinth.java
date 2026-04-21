e0b3ad561ed59b40e089818d66f8dc72fe99e355
saket <saketlovescoding@gmail.com>
2026-04-21 19:14:11 +0530

    Refactor Labyrinth pathfinding: remove unused path variable, add end point tracking, and reconstruct path output

--- real-dsa-squad/graphs/cses/Labyrinth.java
+++ real-dsa-squad/graphs/cses/Labyrinth.java
@@ -8,12 +8,10 @@ import java.util.Stack;
 class IndexPair{
     int x;
     int y;
-    String path;
 
-    public IndexPair(int x, int y, String str){
+    public IndexPair(int x, int y){
         this.x = x;
         this.y=y;
-        path = str;
     }
 }
 
@@ -21,13 +19,14 @@ public class Labyrinth {
 
     private static final int dx[] = new int[]{-1, 0, 0, 1};
     private static final int dy[] = new int[]{0, -1, 1, 0};
+    private static final char direction[] = new char[]{'U', 'L', 'R', 'D'};
     public static void main(String[] args) {
         int n,m;
         Scanner sc = new Scanner(System.in);
         n=sc.nextInt();
         m=sc.nextInt();
         char[][] matrix = new char[n][m];
-        int startX = 0, startY=0;
+        int startX = 0, startY=0, endX=0, endY=0;
         for(int i=0 ; i<n ; i++){
             String st = sc.next();
             for(int j=0 ; j<m ; j++){
@@ -36,51 +35,58 @@ public class Labyrinth {
                     startX = i;
                     startY = j;
                 }
+                if(matrix[i][j]=='B'){
+                    endX=i;
+                    endY=j;
+                }
             }
         }
 
-        String str = tracePath(matrix, startX, startY);
-        if(str.isEmpty()){
-            System.out.println("NO");
-        }else{
-            System.out.println("YES");
-            System.out.println(str.length());
-            System.out.println(str);
-        }
-
-    }
-
-    private static String tracePath(char[][] matrix, int startX, int startY) {
-
+        int[][] pathDirection = new int[matrix.length][matrix[startX].length];
         Queue<IndexPair> st = new LinkedList<>();
-        st.offer(new IndexPair(startX, startY, ""));
+        st.offer(new IndexPair(startX, startY));
         matrix[startX][startY] = '#';
+
+        boolean found=false;
 
         while (!st.isEmpty()){
             int l=st.size();
             for(int k=0 ; k<l ; k++){
                 IndexPair p = st.poll();
                 int i= p.x, j=p.y;
-                String path = p.path;
-                if(matrix[i][j]=='B')return path;
+                if(i==endX && j==endY){
+                    found=true;
+                    break;
+                }
 
                 for(int z=0 ; z<4 ; z++){
                     int x = i+dx[z], y=j+dy[z];
                     if(x>=0 && x<matrix.length && y>=0 && y<matrix[x].length && matrix[x][y]!='#'){
-                        if(matrix[x][y]!='B')matrix[x][y] = '#';
-                        char c;
-                        if(z==0)c='U';
-                        else if(z==1)c='L';
-                        else if(z==2)c='R';
-                        else c='D';
-                        st.offer(new IndexPair(x, y, path+c));
+                        matrix[x][y] = '#';
+                        pathDirection[x][y]=z;
+                        st.offer(new IndexPair(x, y));
                     }
                 }
             }
         }
+        if(!found){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+            StringBuilder sb = new StringBuilder();
+            int currX = endX, currY = endY;
 
+            while (currX!=startX || currY!=startY){
+                int dir = pathDirection[currX][currY];
+                sb.append(direction[dir]);
 
+                currX = currX -dx[dir];
+                currY = currY - dy[dir];
+            }
 
-        return "";
+            System.out.println(sb.toString().length());
+            System.out.println(sb.reverse());
+        }
+
     }
 }
